9c3a82bdda7a2e9f6e141809b9e81a794c7d1e67
Bolgar <bolgar888@gmail.com>
2021-10-10 20:44:10 +0300

    Init commit

--- src/test/java/cloud/autotests/helpers/DriverSettings.java
+++ src/test/java/cloud/autotests/helpers/DriverSettings.java
@@ -1,9 +1,11 @@
 package cloud.autotests.helpers;
 
+import cloud.autotests.config.App;
 import cloud.autotests.config.Project;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,11 +17,11 @@ public class DriverSettings {
         Configuration.browserVersion = Project.config.browserVersion();
         Configuration.browserSize = Project.config.browserSize();
         Configuration.timeout=8000;
-//        Configuration.baseUrl = App.config.webUrl();
+        Configuration.baseUrl = App.config.webUrl();
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         ChromeOptions chromeOptions = new ChromeOptions();
-
+        Configuration.browserCapabilities = capabilities;
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-infobars");
         chromeOptions.addArguments("--disable-popup-blocking");
